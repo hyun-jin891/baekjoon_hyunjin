80381bcef111c731fbd342948fa691920a10260f
Cho, Hyun jin <xkdl27@yonsei.ac.kr>
2024-09-16 15:16:24 +0900

    [Silver IV] Title: 수 찾기, Time: 204 ms, Memory: 136000 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,19 +1,23 @@
-def binary_search(array, target, start, end):
-    while start <= end:
-        mid = (start + end) // 2   
-        if array[mid] == target:
-            return 1
-        elif array[mid] < target:
-            start = mid + 1
+def binary_search(a, x):
+    left = 0
+    right = len(a)
+    
+    while left < right:
+        mid = (left + right) // 2
+        if a[mid] > x:
+            left = mid + 1
+        elif a[mid] < x:
+            right = mid
         else:
-            end = mid - 1
+            return 1
+    
     return 0
 
 N = int(input())
 arr = list(map(int, input().split()))
-arr.sort()
+arr.sort(reverse=True)
 M = int(input())
 num_l = list(map(int, input().split()))
 
 for i in num_l:
-    print(binary_search(arr, i, 0, N-1))
+    print(binary_search(arr, i))
